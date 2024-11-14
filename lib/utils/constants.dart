ee038b7955d72c5ccf89f8b1a12b447ad6d2f81b
smt197 <serignembayet@gmail.com>
2024-11-14 15:51:13 +0000

    url deploiement

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 
 class Constants {
-  static const String baseUrl = "http://192.168.6.44:8000/api";
+  static const String baseUrl = "https://wave-laravel.onrender.com/api";
   static const String apiVersion = "v1";
   static const String authTokenKey = "token";
   static const String refreshTokenKey = "refresh_token";
